4ddf8681cfadf4ce609e77b1236b742e6de968aa
hendellcosta <discordstests@gmail.com>
2023-02-08 14:36:49 +0000

    yeah, message-sent page redirect fixed again

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <main className="flex flex-col items-center justify-center text-center lg:text-start px-6 py-10 gap-20 lg:px-20">
         <Routes>
-          <Route element={<Main />} />
+          <Route path='/' element={<Main />} />
           <Route path="/message-sent" element={<EmailSent />} />
         </Routes>
       </main>
