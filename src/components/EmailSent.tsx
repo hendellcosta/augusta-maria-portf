fbcaae9fffb106170ee3878a2e71d49fbfbcf6fe
hendellcosta <discordstests@gmail.com>
2023-02-08 14:46:33 +0000

    yeah, message-sent page redirect fixed again

--- src/components/EmailSent.tsx
+++ src/components/EmailSent.tsx
@@ -6,7 +6,7 @@ function EmailSent() {
     <div className="flex flex-col items-center justify-center text-center lg:text-start gap-20 text-white h-[100vh]">
       <MdDoneOutline className="text-9xl text-instagram bg-likeBlack rounded-full p-4" />
       <h1 className="text-4xl">A sua mensagem foi enviada com sucesso!</h1>
-      <a href="http://localhost:5173" className="text-blue-700 hover:underline">
+      <a href={window.location.origin} className="text-blue-700 hover:underline">
         Clique aqui para voltar
       </a>
     </div>
